8e71434aceaaa998214c866692193c0720fece13
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-11-21 10:07:01 +0500

    add spm support

--- Tests/LightRouteTests/LightRouteTests.swift
+++ Tests/LightRouteTests/LightRouteTests.swift
@@ -6,7 +6,6 @@ final class LightRouteTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LightRoute().text, "Hello, World!")
     }
 
     static var allTests = [
